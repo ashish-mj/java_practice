5e1b1d88ed8b32cb7b7c8161b5589e93e6574c43
Ashish M J <ashm.jagadeesh@gmail.com>
2021-03-01 11:07:52 +0530

    Add new Excercise - Day 4.1.1

--- part6/Lambda2.java
+++ part6/Lambda2.java
@@ -4,17 +4,31 @@ public class Lambda2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     Greetings g= (s)->"Good morning "+s; 
+      Greetings g= (s)->"Good morning "+s; 
       greetService(g,"Ashish");
       greetService(g,"Darick");
+      
+      Favorite f = (n)-> 10+(int)n;
+      add(f,20.35f);
+      
      }
 	
 	static void greetService(Greetings g,String name) {
 		System.out.println(g.wish(name));
 	}
+	
+	static void add(Favorite f,float n) {
+		System.out.println(f.tell(n));
+	}
+	
+	
 
 }
 
 interface Greetings{
 	String wish(String name);
+}
+
+interface Favorite{
+	int tell(float n);
 }
